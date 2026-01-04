6a1f20a78842463d1323220d0dbbbfd49aafa581
Sam <mybsam12@gmail.com>
2026-01-04 15:06:07 +0800

    add autoconfiguration class and scan the package

---
+++ src/main/java/com/github/justauth/oauth/config/JustAuthAutoConfiguration.java
@@ -0,0 +1,9 @@
+package com.github.justauth.oauth.config;
+
+import org.springframework.boot.autoconfigure.AutoConfiguration;
+import org.springframework.context.annotation.ComponentScan;
+
+@AutoConfiguration
+@ComponentScan("com.github.justauth.oauth")
+public class JustAuthAutoConfiguration {
+}
